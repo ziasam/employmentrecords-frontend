a461b5dcde6c07cd46fd09622774bd00d5386600
Ziauddin Sameer <ziauddinsameer@gmail.com>
2024-06-30 23:44:00 +0600

    initial employment records frontend commit

---
+++ src/app/employee.ts
@@ -0,0 +1,11 @@
+export interface Employee{
+    id: number;
+    name: string;
+    jobTitle: string;
+    stack: string;
+    companyName: string;
+    previousCompany: string;
+    phone: number;
+    imageUrl: string;
+    email: string;   
+}
